61094749be924a8682088899d744aac4e1a268de
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-04-10 17:51:08 +0500

    Сумму между заданными числами

---
+++ HW2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных э
+лементов в промежутке от M до N.*/
+int Promt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+int PrintSumm(int M, int N)
+{
+    if ( M==N ) return M;
+    return M + PrintSumm(M + 1, N);
+}
+
+int N = Promt("Введите N => ");
+int M = Promt("Введите M => ");
+Console.WriteLine($"Сумма чисел равна {PrintSumm(N, M)}");
