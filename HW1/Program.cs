afddc802478aded743d1011d425be417596d7200
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-04-10 17:41:23 +0500

    Вывод цилев в консоль терминала

---
+++ HW1/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задайте значения M и N. Напишите программу, которая выведет все чётные и все нечетные натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8"*/
+int Promt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+void PrintNumber(int M, int N)
+{
+    if (M>N) return;
+    Console.Write(M+" ");
+    PrintNumber(M + 1, N);
+}
+
+void PrintEvenNumber(int M, int N)
+{
+    if (M > N) return;
+    if (M % 2 == 0)
+    Console.Write(M+" ");
+    PrintEvenNumber(M + 1, N);
+}
+
+
+
+void PrintOddNumber(int M, int N)
+{
+    if (M > N) return;
+    if (M % 2 != 0)
+    Console.Write(M+" ");
+    PrintOddNumber(M + 1, N);
+}
+
+int M = Promt("Введите N => ");
+int N = Promt("Введите M => ");
+PrintNumber(M, N);
+System.Console.WriteLine();
+PrintEvenNumber(M, N);
+System.Console.WriteLine();
+PrintOddNumber(M, N);
